48ce62f80a1e4f199734288fd225aeee7a2bd945
Gleb Fandeev <fandeev.business@gmail.com>
2023-11-11 18:37:49 +0300

    @ReuseIdentifier docs

--- Sources/ReuseIdentifiable/ReuseIdentifiable.swift
+++ Sources/ReuseIdentifiable/ReuseIdentifiable.swift
@@ -1,3 +1,20 @@
+/// A macro that generates a `reuseIdentifier` static String field
+///
+///     @ReuseIdentifier
+///     class TableViewCell: UITableViewCell {
+///         ...
+///     }
+///
+///     tableView.register(
+///         TableViewCell.self,
+///         forCellWithReuseIdentifier: TableViewCell.reuseIdentifier
+///     )
+///
+///     let cell = tableView.dequeueReusableCell(
+///         withReuseIdentifier: TableViewCell.reuseIdentifier,
+///         for: indexPath
+///     )
+///
 @attached(member, names: named(reuseIdentifier))
 public macro ReuseIdentifier() = #externalMacro(
     module: "ReuseIdentifiableMacros",
